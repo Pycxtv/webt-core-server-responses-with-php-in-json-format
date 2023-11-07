de59ad781a1c191d06ea8a067838cd29129133d6
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2023-11-07 11:26:24 +0100

    Wrote classes for an OST and a Song

--- src/Song.php
+++ src/Song.php
@@ -1,5 +1,12 @@
 <?php
 
 class Song {
-
+	public function __construct(
+		public int $id,
+		public string $name,
+		public string $artist,
+		public int $trackNumber,
+		public float $duration
+	) {
+	}
 }
