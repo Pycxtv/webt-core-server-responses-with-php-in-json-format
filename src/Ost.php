314668524882269c6be3cbe29f732aaeaaa23dc1
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2023-11-14 08:58:16 +0100

    Installed Faker and removed a comment from Ost.php

--- src/Ost.php
+++ src/Ost.php
@@ -1,7 +1,6 @@
 <?php
 
 class Ost {
-//  unique ID, name, video game name (the one to which it is related to), a release year, and track list consisting of songs
 	public function __construct(
 		public int $id,
 		public string $name,
