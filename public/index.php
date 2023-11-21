d56d4e19456e1e068d43c9b35aae80f0e7ac5595
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2023-11-21 09:31:48 +0100

    Added secret OST logic in index.php

--- public/index.php
+++ public/index.php
@@ -4,10 +4,17 @@ use OnPHPoint\JsonResponses\Seeder;
 header("Content-type: application/json");
 
 $osts = Seeder::generateOSTs(10);
+$secretOst = $osts[count($osts) - 1];
+$osts = array_slice($osts, 0, count($osts) - 1);
 
 $ost_num = $_GET['ost'] ?? null;
 
 if (isset($ost_num)) {
+	if ($ost_num == 666) {
+		echo json_encode($secretOst);
+		return;
+	}
+
     $filtered = array_values(array_filter($osts, function($ost) use ($ost_num) {
         return $ost->id == $ost_num;
     }));
