181d2197075d78cd48d12ac69abbf7bce595c910
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-01-16 09:39:36 +0100

    Corrected OST generation test

--- test/unit/SeederTest.php
+++ test/unit/SeederTest.php
@@ -18,7 +18,7 @@ class SeederTest extends TestCase {
 	}
 
 	protected static function getOSTMethod($name): \ReflectionMethod {
-		$class = new ReflectionClass('Seeder');
+		$class = new ReflectionClass(Seeder::class);
 		return $class->getMethod($name);
 	}
 
@@ -28,14 +28,14 @@ class SeederTest extends TestCase {
 	 */
 	public function testOSTGeneration() {
 		$generateOST = self::getOSTMethod("generateOST");
-		$ost = $generateOST->invoke($this->faker);
+		$ost = $generateOST->invoke($generateOST, $this->faker);
 
 		$this->assertObjectHasProperty("id", $ost);
 		$this->assertInstanceOf(Ost::class, $ost);
-		$this->assertGreaterThan(count($ost->songs), 0);
-		$this->assertGreaterThanOrEqual($ost->releaseYear, 1950);
-		$this->assertIsString($ost->videoGameName, $ost);
-		$this->assertIsString($ost->name, $ost);
+		$this->assertGreaterThan(0, sizeOf($ost->songs));
+		$this->assertGreaterThanOrEqual(1950, $ost->releaseYear);
+		$this->assertIsString($ost->videoGameName);
+		$this->assertIsString($ost->name);
 	}
 
 }
